75026de204c2d9481497ad6025b9e9a0c757ee8e
Malka Haupt <nkfv00@gmail.com>
2025-03-30 20:22:19 +0300

    point +vcector

--- src/unittests/primitives/VectorTests.java
+++ src/unittests/primitives/VectorTests.java
@@ -4,15 +4,78 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import primitives.Vector;
+import primitives.*;
 
 /***
  * 
- *  JUnit tests for primitives.Vector 
+ * JUnit tests for primitives.Vector
  *
-*/
+ */
 class VectorTests {
-	
+
+	Vector v1 = new Vector(2, 3, 4);
+	Vector v2 = new Vector(1, 2, 3);
+	Vector v1Opposite = new Vector(-2, -3, -4);
+	Vector v3 = new Vector(-1, -1, -1);
+	Point p1 = new Point(2, 3, 4);
+	Point P2 = new Point(1, 2, 3);
+	Point P3 = new Point(4, 6, 8);
+
+	/**
+	 * Test method for {@link primitives.Vector#Vector(double, double, double)}.
+	 */
+	@Test
+	void testConstructor() {
+		// ============ Equivalence Partitions Tests ==============
+
+		// TC01: Correct vector with nonzero components
+		assertDoesNotThrow(() -> new Vector(1, 2.9, 3), "Failed to construct a valid vector");
+		assertDoesNotThrow(() -> new Vector(-1.5, -2, -3), "Failed to construct a valid negative vector");
+		assertDoesNotThrow(() -> new Vector(0, 1, 0), "Failed to construct a vector with zero in one coordinate");
+
+		// =============== Boundary Values Tests ==================
+		// TC10: Zero vector (should throw an exception)
+		assertThrows(IllegalArgumentException.class, () -> new Vector(0, 0, 0), "Constructed a zero vector");
+
+	}
+
+	/**
+	 * Test method for {@link primitives.Vector#Vector(Double3)}.
+	 */
+	@Test
+	void testConstructorDouble3() {
+		// ============ Equivalence Partitions Tests ==============
+
+		// TC01: Correct vector with nonzero components
+		assertDoesNotThrow(() -> new Vector(new Double3(1, 2.9, 3))ff, "Failed to construct a valid vector");
+		assertDoesNotThrow(() -> new Vector(new Double3(-1.5, -2, -3)), "Failed to construct a valid negative vector");
+		assertDoesNotThrow(() -> new Vector(new Double3(0, 1, 0)),
+				"Failed to construct a vector with zero in one coordinate");
+
+		// =============== Boundary Values Tests ==================
+		// TC10: Zero vector (should throw an exception)
+		assertThrows(IllegalArgumentException.class, () -> new Vector(new Double3(0)), "Constructed a zero vector");
+
+	}
+
+	/**
+	 * Test method for {@link primitives.Vector#subtract(primitives.Point)}.
+	 */
+	@Test
+	void testSubract() {
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: subtract two vectors positive result
+		assertEquals(new Vector(1, 1, 1), v1.subtract(P2), "ERROR: Vector subtract does not work correctly");
+
+		// TC01:subtract two vectors negative result
+		assertEquals(v1Opposite, v1.subtract(P3), "ERROR: Vector subtract does not work correctly");
+
+		// =============== Boundary Values Tests ==================
+		// TC11:subtract a vector and its opposite should throw an exception
+		assertThrows(IllegalArgumentException.class, () -> v1.subtract(p1),
+				"ERROR: subtract a vector and it same cordidtion point should throw an exception");
+
+	}
 
 	/**
 	 * Test method for {@link primitives.Vector#add(primitives.Vector)}.
@@ -20,13 +83,12 @@ class VectorTests {
 	@Test
 	void testAddVector() {
 
-		Vector v1 = new Vector(2, 3, 4);
-		Vector v2 = new Vector(1, 2, 3);
-
-		Vector v1Opposite = new Vector(-2, -3, -4);
 		// ============ Equivalence Partitions Tests ==============
-		// TC01: Adding two vectors
+		// TC01: Adding two vectors positive result
 		assertEquals(new Vector(3, 5, 7), v1.add(v2), "ERROR: Vector addition does not work correctly");
+
+		// TC01: Adding two vectors negative result
+		assertEquals(new Vector(-1, -1, -1), v1Opposite.add(v2), "ERROR: Vector addition does not work correctly");
 
 		// =============== Boundary Values Tests ==================
 		// TC11: Adding a vector and its opposite should throw an exception
@@ -41,7 +103,6 @@ class VectorTests {
 	@Test
 	void testScale() {
 
-		Vector v1 = new Vector(2, 3, 4);
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: Scaling a vector by a positive scalar
 		assertEquals(new Vector(4, 6, 8), v1.scale(2), "ERROR: Vector scaling does not work correctly");
@@ -61,13 +122,15 @@ class VectorTests {
 	@Test
 	void testDotProduct() {
 
-		Vector v1 = new Vector(2, 3, 4);
-		Vector v2 = new Vector(1, 2, 3);
 		// ============ Equivalence Partitions Tests ==============
-		// TC01: Checking the dot product of two non-orthogonal vectors
+		// TC01: Checking the positive dot product of two non-orthogonal vectors
 		assertEquals(20, v1.dotProduct(v2), "ERROR: Dot product calculation is incorrect");
+		// TC02:Checking the negative dot product of two non-orthogonal vectors
+		assertEquals(-9, v1.dotProduct(v3), "ERROR: Dot product calculation is incorrect");
 
-		// TC02: Checking the dot product of orthogonal vectors
+		// =============== Boundary Values Tests ==================
+
+		// TC10: Checking the dot product of orthogonal vectors
 		assertEquals(0, new Vector(1, 0, 0).dotProduct(new Vector(0, 1, 0)),
 				"ERROR: Dot product of orthogonal vectors should be zero");
 	}
@@ -78,8 +141,6 @@ class VectorTests {
 	@Test
 	void testCrossProduct() {
 
-		Vector v1 = new Vector(2, 3, 4);
-		Vector v2 = new Vector(1, 2, 3);
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: Checking the cross product of two non-parallel vectors
 		Vector cross = v1.crossProduct(v2);
@@ -97,13 +158,10 @@ class VectorTests {
 	@Test
 	void testLengthSquared() {
 
-		Vector v1 = new Vector(2, 3, 4);
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: Checking the squared length of a vector
 		assertEquals(29, v1.lengthSquared(), "ERROR: Length squared calculation is incorrect");
 		// =============== Boundary Values Tests ==================
-	
-
 
 	}
 
@@ -113,7 +171,6 @@ class VectorTests {
 	@Test
 	void testLength() {
 
-		Vector v1 = new Vector(2, 3, 4);
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: Checking the length of a vector
 		assertEquals(Math.sqrt(29), v1.length(), "ERROR: Length calculation is incorrect");
