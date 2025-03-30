ed60d840bd45f8b76d77934ea8c666a8ef1d775a
Malka Haupt <nkfv00@gmail.com>
2025-03-30 23:30:09 +0300

    stage 2:fix sphere

--- src/unittests/geometries/SphereTests.java
+++ src/unittests/geometries/SphereTests.java
@@ -24,7 +24,7 @@ class SphereTests {
 
 		// TC01: Correct normal to the Sphere
 		Sphere sphere = new Sphere(new Point(1, 1, 1), 10.0);
-		assertEquals(new Vector(0, 0, 1), sphere.getNormal(new Point(1, 1, 11)), "get Normal doen't work as expected ");
+		assertEquals(1, sphere.getNormal(new Point(1, 1, 11)).length(), "get Normal doen't work as expected ");
 	}
 
 }
